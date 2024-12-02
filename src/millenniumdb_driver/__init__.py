4c167f8b7f94ddb06af87811336fbe8576f38af0
m-hadz <matiasmaldonado@uc.cl>
2024-12-02 12:22:56 -0300

    minor fixes

--- src/millenniumdb_driver/__init__.py
+++ src/millenniumdb_driver/__init__.py
@@ -1,8 +1,9 @@
 from .driver import Driver as _Driver
 from .millenniumdb_error import MillenniumDBError, ResultError
+import importlib.metadata
 
 
-__version__ = "0.0.1"
+__version__ = importlib.metadata.version("millenniumdb_driver")
 
 
 def driver(url: str) -> _Driver:
